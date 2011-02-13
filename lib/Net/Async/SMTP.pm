9f0a824a6a1ba45c909bcc2730e76e9b6efd7c77
Tom Molesworth <tom@perlsite.co.uk>
2011-02-13 23:38:43 +0000

    Initial base structure for SMTP
    git-svn-id: http://entitymodel.com/svn/trunk/Net-Async-SMTP@739 c5bf8c3b-bf20-4ed3-b413-d5547a49f493

--- lib/Net/Async/SMTP.pm
+++ lib/Net/Async/SMTP.pm
@@ -1,13 +1,61 @@
 package Net::Async::SMTP;
-# ABSTRACT: Do stuff
 use strict;
-use warnings FATAL => 'all', NONFATAL => 'redefine';
-use 5.10.0;
+use warnings;
 
-our $VERSION = '1.000';
-
-=pod
-
-=cut
+our $VERSION = '0.001';
 
 1;
+
+__END__
+
+=head1 NAME
+
+Net::Async::SMTP - asynchronous SMTP handling based on L<Protocol::SMTP> and L<IO::Async::Protocol::Stream>.
+
+=head1 SYNOPSIS
+
+ use IO::Async::Loop;
+ use Net::Async::SMTP;
+ my $loop = IO::Async::Loop->new;
+ my $imap = Net::Async::SMTP::Client->new(
+ 	loop => $loop,
+	host => 'mailserver.com',
+	service => 1025, # custom port number example
+	user => 'user@mailserver.com',
+	pass => 'password',
+
+# Automatically retrieve any new messages that arrive on the server
+	on_send_ready => sub {
+		my ($self, $id) = @_;
+		$self->send(
+			from		=> '',
+			recipient	=> '',
+			data		=> sub {
+
+			}
+		);
+	},
+
+# Display the subject whenever we have a successful FETCH command
+	on_queued => sub {
+		my ($self, $id) = @_;
+		warn "Queued as $msg";
+	},
+ );
+ $loop->loop_forever;
+
+=head1 DESCRIPTION
+
+Provides support for communicating with SMTP servers under L<IO::Async>.
+
+See L<Protocol::SMTP> for more details on this implementation of SMTP, and RFC822 and similar
+for the official protocol specification.
+
+=head1 AUTHOR
+
+Tom Molesworth <cpan@entitymodel.com>
+
+=head1 LICENSE
+
+Copyright Tom Molesworth 2011. Licensed under the same terms as Perl itself.
+
